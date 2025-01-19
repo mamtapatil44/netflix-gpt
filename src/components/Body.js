8746594daa7ed41d14900c9aed8611e2a7d372e6
Mamta Patil <99733178+mamtapatil44@users.noreply.github.com>
2025-01-18 21:38:50 -0500

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Login from "./Login";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/userSlice";
+
 
 const routes = createBrowserRouter([
   { path: "/", element: <Login /> },
